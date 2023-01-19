d690f466ee77e6c8c700c9cf367ba6e0fdf8311c
zoey-t <101599447+zoey-t@users.noreply.github.com>
2023-01-19 12:44:33 -0800

    cargo fix to remove unsued import

--- tests/local_tests/token_init.rs
+++ tests/local_tests/token_init.rs
@@ -1,4 +1,4 @@
-use crate::utils::local_test_utils::abi_calls::{decimals, initialize, name, owner, symbol};
+use crate::utils::local_test_utils::abi_calls::{decimals, name, owner, symbol};
 use crate::utils::local_test_utils::setup_utils::setup_token;
 
 use fuels::prelude::*;
